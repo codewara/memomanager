e20ec9457316dc362ddfbe6e5009b9ccec0109f9
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-09 19:15:57 +0700

    loader now considering disk manager and fix modified time

--- src/Model/FileLoader.java
+++ src/Model/FileLoader.java
@@ -6,25 +6,26 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.io.File;
 
 public class FileLoader {
-    public static Directory loadFromJSON(File file) throws Exception {
+    public static Directory loadFromJSON(File file, DiskManager diskManager) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(file);
-        return (Directory) parseDir(root, null);
+        return (Directory) parseDir(root, null, diskManager);
     }
 
-    private static SystemNode parseDir(JsonNode node, Directory parent) {
-        if (!"directory".equals(node.get("type").asText())) {
-            throw new IllegalArgumentException("Expected a directory node");
-        }
+    private static SystemNode parseDir(JsonNode node, Directory parent, DiskManager diskManager) {
+        if (!"directory".equals(node.get("type").asText())) throw new IllegalArgumentException("Expected a directory node");
 
-        Directory dir = new Directory(node.get("name").asText(), parent);
+        Directory dir = new Directory(node.get("name").asText(), parent, node.get("modified").asText(""));
         for (JsonNode childNode : node.get("children")) {
             String type = childNode.get("type").asText();
-            if ("directory".equals(type)) dir.addChild(parseDir(childNode, dir));
+            if ("directory".equals(type)) dir.addChild(parseDir(childNode, dir, diskManager));
             else if ("file".equals(type)) {
                 String name = childNode.get("name").asText();
                 String content = childNode.get("content").asText("");
-                dir.addChild(new Model.File(name, dir, content));
+                String modifiedTime = childNode.get("modified").asText("");
+                Model.File file = new Model.File(name, dir, content, modifiedTime);
+                diskManager.allocateContiguous(file.getContent());
+                dir.addChild(file);
             }
         }
         return dir;
