ff02a8768aaaafc8342b1a82ea76c271df126b2c
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-10 04:41:36 +0700

    use actual file for resource

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             try {
-                File json = new File(Objects.requireNonNull(App.class.getClassLoader().getResource("resources/structure.json")).toURI());
+                File json = new File("src/resources/structure.json");
                 new SystemController().start(json);
             } catch (Exception e) { e.printStackTrace(); }
         });
