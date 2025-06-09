400fd7707550c6c52b631b7da83e04206939dc72
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-10 02:45:05 +0700

    add new attr.: allocatedblocks in file

--- src/Model/File.java
+++ src/Model/File.java
@@ -1,16 +1,18 @@
 package Model;
 
-import java.util.Date;
+import java.util.*;
 import java.text.SimpleDateFormat;
 
 public class File extends SystemNode {
+    private List<Integer> allocatedBlocks = new ArrayList<>();
     private String content;
     private int size;
 
-    public File(String name, Directory parent, String content, String modifiedTime) {
+    public File(String name, Directory parent, String content, String modifiedTime, List<Integer> allocatedBlocks) {
         super(name, parent, modifiedTime);
         this.content = content;
         this.size = content != null ? content.length() : 0;
+        this.allocatedBlocks = new ArrayList<>(allocatedBlocks);
     }
 
     public String getContent() { return content; }
@@ -18,6 +20,11 @@ public class File extends SystemNode {
         this.content = content;
         this.size = content != null ? content.length() : 0;
         setModifiedTime(new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss").format(new Date()));
+    }
+
+    public Set<Integer> getUsedBlocks() { return new HashSet<>(allocatedBlocks); }
+    public void setUsedBlocks(List<Integer> blocks) {
+        this.allocatedBlocks = new ArrayList<>(blocks);
     }
 
     @Override public boolean isDirectory() { return false; }
